fd80b54d0fca5cb0d7194a76c129554c9b0b8942
hikkathon <hikkathon@gmail.com>
2020-04-06 17:58:32 +0700

    up

--- Webscraper/Webscraper/MainWindow.xaml.cs
+++ Webscraper/Webscraper/MainWindow.xaml.cs
@@ -20,9 +20,23 @@ namespace Webscraper
     /// </summary>
     public partial class MainWindow : Window
     {
+        Scraper scraper;
+
         public MainWindow()
         {
             InitializeComponent();
+            scraper = new Scraper();
+            DataContext = scraper;
+        }
+
+        private void BtnScraper_Click(object senedr, RoutedEventArgs e)
+        {
+            scraper.ScrapeData(TbPage.Text);
+        }
+
+        private void MenuItem_Click(object senedr, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Webscraper/Webscraper/Scraper.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Webscraper
+{
+    public class Scraper
+    {
+        private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        public ObservableCollection<EntryModel> Entries
+        {
+            get { return _entries; }
+            set { _entries = value; }
+        }
+
+        public void ScrapeData(string page)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(page);
+
+            var Articles = doc.DocumentNode.SelectNodes("//*[@class='article-single']");
+
+            foreach(var article in Articles)
+            {
+                var header = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class='article-header']")?.InnerText);
+                var description = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class='article-copy']")?.InnerText);
+
+                _entries.Add(new EntryModel { Title = header, Description = description });
+            }
+        }
+    }
+}
